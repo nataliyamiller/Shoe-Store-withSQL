e761a8c3f1b6ec927abb0b76c2a42cf77d027cc0
Nataliya Miller <nataliyamiller82@gmail.com>
2015-09-02 20:12:04 -0700

    Store constructor instantiates correctly, test passes

--- src/main/java/Store.java
+++ src/main/java/Store.java
@@ -3,6 +3,26 @@ import org.sql2o.*;
 import java.util.ArrayList;
 
 public class Store {
+  private String name;
+  private String address;
+  private String phone_number;
 
+  public String getName() {
+    return name;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getPhoneNumber() {
+    return phone_number;
+  }
+
+  public Store (String name, String address, String phoneNumber) {
+    this.name = name;
+    this.address = address;
+    this.phone_number = phoneNumber;
+  }
 
 }
--- src/test/java/StoreTest.java
+++ src/test/java/StoreTest.java
@@ -7,8 +7,14 @@ import java.util.ArrayList;
 
 public class StoreTest {
 
-     @Rule
-     public DatabaseRule database = new DatabaseRule();
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void store_instantiatesCorrectly_true() {
+    Store testStore = new Store("Target", "Portland, OR", "503-555-5555");
+    assertTrue(testStore instanceof Store);
+  }
 
 
  }
