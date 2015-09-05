9cdab375ee8c6351839eff4e16bfa25ec22c4a40
Nataliya Miller <nataliyamiller82@gmail.com>
2015-09-05 14:11:18 -0700

    A little styling for home,stores,brands pages

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -25,7 +25,7 @@ public class AppTest extends FluentTest{
   @Test
   public void rootTest() {
     goTo("http://localhost:4567/");
-    assertThat(pageSource()).contains("Shoe stores");
+    assertThat(pageSource()).contains("Shoe Stores and Brands");
   }
 
   @Test
