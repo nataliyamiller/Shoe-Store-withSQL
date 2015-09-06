6e09b62a31cf05a137e31cd981ee0d005d15790b
Nataliya Miller <nataliyamiller82@gmail.com>
2015-09-05 21:34:00 -0700

    All tests are passing for the refactored method

--- src/main/java/Store.java
+++ src/main/java/Store.java
@@ -125,26 +125,6 @@ public void updatePhoneNumber(String phone_number) {
       }
 
 
-  // public ArrayList<Brand> getBrands() {
-  //   try(Connection con = DB.sql2o.open()) {
-  //     String sql = "SELECT brand_id FROM stores_brands WHERE store_id =:store_id";
-  //     List<Integer> brandIds = con.createQuery(sql)
-  //       .addParameter("store_id", this.getId())
-  //       .executeAndFetch(Integer.class);
-  //
-  //       ArrayList<Brand> brands = new ArrayList<Brand>();
-  //
-  //     for(Integer brandId : brandIds) {
-  //       String brandQuery = "SELECT * FROM brands WHERE id=:brandId";
-  //       Brand brand = con.createQuery(brandQuery)
-  //         .addParameter("brandId", brandId)
-  //         .executeAndFetchFirst(Brand.class);
-  //       brands.add(brand);
-  //     }
-  //     return brands;
-  //   }
-  // }
-
   public void delete() {
     try(Connection con = DB.sql2o.open()) {
       String deleteQuery = "DELETE FROM stores WHERE id = :id;";
